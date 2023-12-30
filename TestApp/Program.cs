d6b80db9a272ae69e85adc00a6abcb8efc48e453
Michael Green <84688932+michael-j-green@users.noreply.github.com>
2023-12-30 15:35:58 +1100

    Add TestApp and fixed GitHub Actions

---
+++ TestApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HasheousClient.Models;
+
+HasheousClient.WebApp.HttpHelper.BaseUri = "https://hasheous.org/";
+
+HasheousClient.Hasheous hasheous = new HasheousClient.Hasheous();
+
+SignatureLookupItem? HasheousResult = hasheous.RetrieveFromHasheousAsync(new HashLookupModel{
+    MD5 = "93bb1e1ee14ac53adb9e6b9e0c135043"
+});
+
+if (HasheousResult != null)
+{
+    string outString = Newtonsoft.Json.JsonConvert.SerializeObject(HasheousResult, Newtonsoft.Json.Formatting.Indented);
+    Console.WriteLine(outString);
+}
--- hasheous-client/HasheousClient.cs
+++ hasheous-client/HasheousClient.cs
@@ -4,14 +4,14 @@ namespace HasheousClient
 {
     public class Hasheous
     {
-        public static SignatureLookupItem RetrieveFromHasheousAsync(HashLookupModel hash)
+        public SignatureLookupItem RetrieveFromHasheousAsync(HashLookupModel hash)
         {
             Task<SignatureLookupItem> result = _RetrieveFromHasheousAsync(hash);   
 
             return result.Result;
         }
 
-        private static async Task<SignatureLookupItem> _RetrieveFromHasheousAsync(HashLookupModel hashLookup)
+        private async Task<SignatureLookupItem> _RetrieveFromHasheousAsync(HashLookupModel hashLookup)
         {
             var result = await HasheousClient.WebApp.HttpHelper.Post<SignatureLookupItem>($"/api/v1/HashLookup/Lookup", hashLookup);
 
